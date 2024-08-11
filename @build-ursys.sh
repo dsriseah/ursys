b8197f60d45c32eccf223f24461ec5384b7e61b3
DSri Seah <11952933+dsriseah@users.noreply.github.com>
2024-08-11 16:12:19 -0400

    ursys-framework: root level @build-ursys.sh now looks for script directory in parent(s) if not in cwd

--- @build-ursys.sh
+++ @build-ursys.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# directory of this script
-DIR=$(cd "$(dirname "$0")"; pwd)
-DIR="$DIR/_ur/npm-scripts"
-
 # TERMINAL COLORS
 YEL=$(tput setaf 3)
 BLU=$(tput setaf 4)
@@ -12,6 +8,20 @@ BRI=$(tput bold)
 DIM=$(tput dim)
 RST=$(tput sgr0)
 
+# DIRECTORY DETECTION
+CPWD=$(pwd)
+# root directory has the .nvmrc file in it
+ROOT=$(pwd)
+while [ ! -f "$ROOT/.nvmrc" ]; do
+    ROOT=$(dirname "$ROOT")
+    if [ "$ROOT" = "/" ]; then
+        echo "ERROR: .nvmrc file not found"
+        exit 1
+    fi
+done
+# path to the npm-scripts directory relative to ROOT
+DIR="$ROOT/_ur/npm-scripts"
+
 # BUILD OPTIONS
 TSOPTS="--transpile-only" # use --no-cache to force a full rebuild tsnode (disables typecheck)
 
@@ -19,12 +29,17 @@ TSOPTS="--transpile-only" # use --no-cache to force a full rebuild tsnode (disab
 GIT_D=$(git log -1 --format=%cd --date=format:%Y/%m/%d)
 GIT_B=$(git branch --show-current)
 printf "${DIM}building URSYS Library from branch ${RST}${GIT_B}${DIM} commit date ${RST}${GIT_D}${RST}\n"
-
 printf "${DIM}building URSYS Library from branch ${RST}${GIT_B}${DIM} commit date ${RST}${GIT_D}${RST}\n"
+# change to root directory of the project before runing ts-node-esm
+cd $ROOT
+printf "${DIM}--> cd ${ROOT}${RST}\n"
 # (1) always build library first
 npx ts-node-esm $TSOPTS $DIR/@build-core.mts 2>&1 | cat
 # (2) add additional tasks here (eventually can be command args)
 npx ts-node-esm $TSOPTS $DIR/@build-addons.mts 2>&1 | cat
-exit 0;
-;;
+# return to the original directory
+cd - 2>&1 >/dev/null
+printf "${DIM}<-- cd ${CPWD}${RST}\n"
+
+
 
